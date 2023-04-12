11853e8b07141b9bbc1dbaa7fd634cb3c15bb6b9
19dlam92 <19dlam92@gmail.com>
2023-04-12 13:05:50 -0400

    including infinite scrolling

--- client/src/features/infinite-scrolling.jsx
+++ client/src/features/infinite-scrolling.jsx
@@ -8,7 +8,28 @@ const InfiniteScrolling = () => {
 
   return(
     <>
-    
+      <InfiniteScroll
+        dataLength={ items.length } //This is important field to render the next data
+        next={ fetchData }
+        hasMore={ true }
+        loader={ <h4>Loading...</h4> }
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+        refreshFunction={ this.refresh }
+        pullDownToRefresh
+        pullDownToRefreshThreshold={ 50 }
+        pullDownToRefreshContent={
+          <h3 style={{ textAlign: 'center' }}>&#8595; Pull down to refresh</h3>
+        }
+        releaseToRefreshContent={
+          <h3 style={{ textAlign: 'center' }}>&#8593; Release to refresh</h3>
+        }
+      >
+        { items }
+      </InfiniteScroll>
     </>
   )
 }
