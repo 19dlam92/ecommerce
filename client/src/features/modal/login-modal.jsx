7c0ec39b0f3db9495632d00bb8141a1af089fe80
19dlam92 <19dlam92@gmail.com>
2023-04-13 15:53:39 -0400

    modifying modals

--- client/src/features/modal/login-modal.jsx
+++ client/src/features/modal/login-modal.jsx
@@ -5,50 +5,76 @@ import Modal from 'react-bootstrap/Modal'
 
 const LoginModal = () => {
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [formErrors, setFormErrors] = useState({});
+  const history = useHistory();
   const [open, setOpen] = useState(false);
 
   const handleClose = () => setOpen(false);
   const handleOpen = () => setOpen(true);
 
 
+  const loggedIn = (e) => {
+    e.preventDefault()
+    const loggedInUser = {
+      email,
+      password
+    }
+    axios.post('http://localhost:8000/api/User/login', loggedInUser, { withCredentials: true })
+      .then( res => {
+        console.log('Response for login.jsx post method', res)
+        if ( res.data.errors ) {
+          setFormErrors( res.data.errors )
+        } else {
+          history.push('/dashboard')
+        }
+      })
+      .catch( err => {
+        console.error( err )
+      })
+  }
+
   return(
     <>
-      <Button variant="primary" onClick={ handleOpen }>
-        Log In
-      </Button>
-      <Modal show={ open } onHide={ handleClose }>
-        <Modal.Header closeButton>
-          <Modal.Title>Login</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="loginForm.ControlInput1">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="name@example.com"
-                autoFocus
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="loginForm.ControlInput1">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="*************"
-                autoFocus
-              />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="primary" onClick={ handleClose }>
-            Login
-          </Button>
-          <Button variant="secondary" onClick={ handleClose }>
-            Cancel
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      <form onSubmit={ loggedIn }>
+        <Button variant="primary" onClick={ handleOpen }>
+          Log In
+        </Button>
+        <Modal show={ open } onHide={ handleClose }>
+          <Modal.Header closeButton>
+            <Modal.Title>Login</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3" controlId="loginForm.ControlInput1">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="name@example.com"
+                  autoFocus
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="loginForm.ControlInput1">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="*************"
+                  autoFocus
+                />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={ handleClose }>
+              Login
+            </Button>
+            <Button variant="secondary" onClick={ handleClose }>
+              Cancel
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </form>
     </>
   )
 }
--- client/src/features/modal/register-modal.jsx
+++ client/src/features/modal/register-modal.jsx
@@ -5,58 +5,86 @@ import Modal from 'react-bootstrap/Modal'
 
 const RegisterModal = () => {
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirm, setConfirm] = useState("");
+  const [formErrors, setFormErrors] = useState({});
+  const history = useHistory();
   const [open, setOpen] = useState(false);
 
   const handleClose = () => setOpen(false);
   const handleOpen = () => setOpen(true);
 
+  const registered = (e) => {
+    e.preventDefault()
+    const registerInfo = {
+      email,
+      password,
+      confirm
+    }
+    axios.post('http://localhost:8000/api/User/register', registerInfo, { withCredentials: true })
+      .then( res => {
+        if ( res.data.errors ) {
+          console.log('Response for register.jsx post method', res)
+          setFormErrors( res.data.errors )
+        } else {
+          history.push('/login')
+        }
+      })
+      .catch( err => {
+        console.error( err )
+      })
+  }
+
 
   return(
     <>
-      <Button variant="primary" onClick={ handleOpen }>
-        Register
-      </Button>
-      <Modal show={ open } onHide={ handleClose }>
-        <Modal.Header closeButton>
-          <Modal.Title>Create Account</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="registerForm.ControlInput1">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="name@example.com"
-                autoFocus
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="registerForm.ControlInput1">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="*************"
-                autoFocus
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="registerForm.ControlInput1">
-              <Form.Label> Confirm Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="*************"
-                autoFocus
-              />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="primary" onClick={ handleClose }>
-            Register
-          </Button>
-          <Button variant="secondary" onClick={ handleClose }>
-            Cancel
-          </Button>
-        </Modal.Footer>
-      </Modal>
+    <form onSubmit={ registered }>
+        <Button variant="primary" onClick={ handleOpen }>
+          Register
+        </Button>
+        <Modal show={ open } onHide={ handleClose }>
+          <Modal.Header closeButton>
+            <Modal.Title>Create Account</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3" controlId="registerForm.ControlInput1">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="name@example.com"
+                  autoFocus
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="registerForm.ControlInput1">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="*************"
+                  autoFocus
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="registerForm.ControlInput1">
+                <Form.Label> Confirm Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="*************"
+                  autoFocus
+                />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={ handleClose }>
+              Register
+            </Button>
+            <Button variant="secondary" onClick={ handleClose }>
+              Cancel
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </form>
     </>
   )
 }
